9aa45922af29f39b802caf19cadc2b152552422d
n1ternr <trueno76251@gmail.com>
2025-08-06 12:16:09 +0330

    Add files via upload

--- odd or even number detector.py
+++ odd or even number detector.py
@@ -1,5 +1,6 @@
-user_num = int(input("choose number: "))
-if ((user_num % 2) == 0):
-    print("Your number is even")
-else:
-    print("Your number is odd")
+def odd_even_detector(user_num):    
+    if ((user_num % 2) == 0):
+        print("Your number is even")
+    else:
+        print("Your number is odd")
+odd_even_detector(int(input("choose number: ")))        
